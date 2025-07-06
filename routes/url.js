daacb6ea0e86d92641b453866d433ff15fc45569
Yash Shaw <yshaw2002@gmail.com>
2025-07-07 02:19:46 +0530

    Complted POST route

--- routes/url.js
+++ routes/url.js
@@ -1,4 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const {handleGenerateNewShortURL} = require('../controllers/url.js')
 
-router.post('/')
+router.post('/',handleGenerateNewShortURL)
+
+module.exports= router;
