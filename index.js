1023836d16f480c9d4b534e034eac84208e51358
Yash Shaw <yshaw2002@gmail.com>
2025-07-07 02:37:52 +0530

    POST route working & connected to DB

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const urlRoute = require('./routes/url.js')
+const {connectToMongoDB} = require('./connect.js')
 
 const PORT =8001
 const app = express()
+app.use(express.json())
+
+connectToMongoDB("mongodb://127.0.0.1:27017/short-url")
+.then(()=>{
+    console.log("MongoDB Connected");
+})
 
 app.use('/url',urlRoute)
 
