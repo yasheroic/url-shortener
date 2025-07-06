646dd4f0522cc1ac5f1459ec4edbcba4e3fda974
Yash Shaw <yshaw2002@gmail.com>
2025-07-07 02:26:55 +0530

    connection for MongoDB

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const urlRoute = require('./routes/url.js')
 
 const PORT =8001
-
 const app = express()
+
+app.use('/url',urlRoute)
 
 
 app.listen(PORT,()=>console.log(`App is listening on PORT: ${PORT}`))
