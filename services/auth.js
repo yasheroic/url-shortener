84dde60c01c673b1a35228ffdfd6ab4e17c5a1da
Yash Shaw <yshaw2002@gmail.com>
2025-07-11 23:56:22 +0530

    jwt implemented

--- services/auth.js
+++ services/auth.js
@@ -1,10 +1,14 @@
-const sessionIdToUserMap = new Map()
-
-function setUser(id,user){
-    sessionIdToUserMap.set(id,user)
+const jwt = require("jsonwebtoken")
+const secret = "secr3t"
+function setUser(user){
+    return jwt.sign({
+        _id: user._id,
+        email:user.email
+    },secret)
 }
-function getUser(id){
-    return sessionIdToUserMap.get(id)
+function getUser(token){
+    if(!token) return null
+    return jwt.verify(token,secret)
 }
 
 module.exports = {
